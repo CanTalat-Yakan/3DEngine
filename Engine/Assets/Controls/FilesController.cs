4f86c4b67a633e8fa7925ec0748367c289b0967f
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2022-11-03 21:08:21 +0100

    Files now lists categories and contain all necessary informations

--- Engine/Assets/Controls/FilesController.cs
+++ Engine/Assets/Controls/FilesController.cs
@@ -4,13 +4,61 @@ using Microsoft.UI.Xaml.Media;
 using Windows.UI;
 using System;
 using Microsoft.UI;
-using System.Reflection.Emit;
+using System.IO;
+using System.Drawing.Drawing2D;
+using CommunityToolkit.WinUI.UI.Controls;
 
 namespace Editor.Controls
 {
+    internal struct Category
+    {
+        public string Name;
+        public Symbol Symbol;
+        public string Glyph;
+        public bool DefaultColor;
+        public string[] SupportedFileTypes;
+    }
+
     internal class FilesController
     {
-        public Grid CategoryTile(string s, string glyph, bool rndColor = true)
+        public string RootPath { get; private set; }
+        public string CurrentProjectTitle { get; private set; }
+        public WrapPanel Wrap;
+        public Category[] Categories;
+
+        public FilesController(WrapPanel wrap)
+        {
+            Wrap = wrap;
+
+            var documentsDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            RootPath = Path.Combine(documentsDir, "3DEngine");
+
+            if (!string.IsNullOrEmpty(CurrentProjectTitle))
+                RootPath = Path.Combine(RootPath, CurrentProjectTitle);
+        }
+
+        public void ValidateCategoriesExist()
+        {
+            string subFolderPath;
+
+            foreach (var info in Categories)
+                if (!Directory.Exists(subFolderPath = Path.Combine(RootPath, info.Name)))
+                    Directory.CreateDirectory(subFolderPath);
+        }
+
+        public void CreateCatergories(params Category[] categories)
+        {
+            Categories = categories;
+
+            foreach (var info in Categories)
+                if (string.IsNullOrEmpty(info.Glyph))
+                    Wrap.Children.Add(CategoryTile(info.Name, info.Symbol, !info.DefaultColor));
+                else
+                    Wrap.Children.Add(CategoryTile(info.Name, info.Glyph, !info.DefaultColor));
+        }
+
+        private Grid CategoryTile(string s, string glyph, bool rndColor = true)
         {
             Grid grid = new Grid();
             FontIcon icon = new FontIcon() { FontFamily = new FontFamily("Segoe MDL2 Assets"), Glyph = glyph };
@@ -23,7 +71,7 @@ namespace Editor.Controls
             return CategoryTile(s, grid, rndColor);
         }
 
-        public Grid CategoryTile(string s, Symbol symbol, bool rndColor = true)
+        private Grid CategoryTile(string s, Symbol symbol, bool rndColor = true)
         {
             Grid grid = new Grid();
             SymbolIcon symbolIcon = new SymbolIcon() { Symbol = symbol };
@@ -75,5 +123,7 @@ namespace Editor.Controls
 
             return grid;
         }
+
+
     }
 }
--- Engine/UserControls/Files.xaml.cs
+++ Engine/UserControls/Files.xaml.cs
@@ -11,7 +11,7 @@ namespace Editor.UserControls
     {
         public Grid ChangeColorWithTheme;
 
-        internal FilesController _filesControl = new FilesController();
+        internal FilesController _filesControl;
 
         public Files()
         {
@@ -19,18 +19,24 @@ namespace Editor.UserControls
 
             ChangeColorWithTheme = x_Grid_ChangeColorWithTheme;
 
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Scenes", "\xEA86"));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Scripts", Symbol.Document));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Prefabs", "\xE734"));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Models", "\xF158"));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Animation", "\xE805"));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Materials", "\xF156"));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Textures", "\xEB9F"));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Audio", Symbol.Audio));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Videos", Symbol.Video));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Fonts", Symbol.Font));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Documents", "\xEA90"));
-            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Packages", "\xE74C", false));
+            _filesControl = new FilesController(x_WrapPanel_Files);
+
+            _filesControl.CreateCatergories(
+                new Category() { Name = "Scene", Glyph = "\xEA86", SupportedFileTypes = new string[] { ".usd", ".usda", ".usdc", ".usdz" } },
+                new Category() { Name = "Scripts", Symbol = Symbol.Document, SupportedFileTypes = new string[] { ".cs" } },
+                new Category() { Name = "Prefabs", Glyph = "\xE734", SupportedFileTypes = new string[] { ".prefab" } },
+                new Category() { Name = "Models", Glyph = "\xF158", SupportedFileTypes = new string[] { ".fbx", ".obj", ".blend", ".3ds", ".dae" } },
+                new Category() { Name = "Animation", Glyph = "\xE805", SupportedFileTypes = new string[] { ".fbx", ".dae" } },
+                new Category() { Name = "Materials", Glyph = "\xF156", SupportedFileTypes = new string[] { ".material" } },
+                new Category() { Name = "Textures", Glyph = "\xEB9F", SupportedFileTypes = new string[] { ".png", ".jpeg", ".tiff", ".tga", ".psd", ".bmp", } },
+                new Category() { Name = "Audio", Symbol = Symbol.Audio, SupportedFileTypes = new string[] { ".m4a", ".mp3", ".wav", ".ogg" } },
+                new Category() { Name = "Videos", Symbol = Symbol.Video, SupportedFileTypes = new string[] { ".m4v", ".mp4", ".mov", ".avi" } },
+                new Category() { Name = "Fonts", Symbol = Symbol.Font, SupportedFileTypes = new string[] { ".ttf", ".otf" } },
+                new Category() { Name = "Documents", Glyph = "\xEA90", SupportedFileTypes = new string[] { ".pdf", ".txt", ".doc", ".docx" } },
+                new Category() { Name = "Packages", Glyph = "\xE74C", DefaultColor = true, SupportedFileTypes = new string[] { ".zip", ".7zip", ".winrar" } });
+
+            _filesControl.ValidateCategoriesExist();
         }
+
     }
 }
