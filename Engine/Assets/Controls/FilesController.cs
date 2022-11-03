56f639dea535393ba1c66ea19939281aa9d6bf9a
Can_Yakan <can.talat.yakan@gmail.com>
2022-11-03 06:04:51 +0100

    Made Catergory Tiles dynamic in script

--- Engine/Assets/Controls/FilesController.cs
+++ Engine/Assets/Controls/FilesController.cs
@@ -1,12 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI;
+using System;
+using Microsoft.UI;
+using System.Reflection.Emit;
 
-namespace Engine.Assets.Controls
+namespace Editor.Controls
 {
     internal class FilesController
     {
+        public Grid CategoryTile(string s, string glyph, bool rndColor = true)
+        {
+            Grid grid = new Grid();
+            FontIcon icon = new FontIcon() { FontFamily = new FontFamily("Segoe MDL2 Assets"), Glyph = glyph };
+
+            if (rndColor)
+                icon.Foreground = new SolidColorBrush(Colors.White);
+
+            grid.Children.Add(icon);
+
+            return CategoryTile(s, grid, rndColor);
+        }
+
+        public Grid CategoryTile(string s, Symbol symbol, bool rndColor = true)
+        {
+            Grid grid = new Grid();
+            SymbolIcon symbolIcon = new SymbolIcon() { Symbol = symbol };
+
+            if (rndColor)
+                symbolIcon.Foreground = new SolidColorBrush(Colors.White);
+
+            grid.Children.Add(symbolIcon);
+
+            return CategoryTile(s, grid, rndColor);
+        }
+
+        private Grid CategoryTile(string s, Grid symbol, bool rndColor = true)
+        {
+            Grid grid = new Grid();
+            Button btn = new Button()
+            {
+                Width = 150,
+                Height = 100,
+                CornerRadius = new CornerRadius(10),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            if (rndColor)
+                btn.Background = new SolidColorBrush(new Color()
+                {
+                    A = 255,
+                    R = (byte)new Random().Next(64, 192),
+                    B = (byte)new Random().Next(64, 192),
+                    G = (byte)new Random().Next(64, 192)
+                });
+
+            StackPanel stack = new StackPanel() { Spacing = 20 };
+            Viewbox viewbox = new Viewbox() { MaxHeight = 24, MaxWidth = 24 };
+
+            TextBlock label = new TextBlock() { Text = s };
+
+            if (rndColor)
+                label.Foreground = new SolidColorBrush(Colors.White);
+
+            viewbox.Child = symbol;
+
+            stack.Children.Add(viewbox);
+            stack.Children.Add(label);
+
+            btn.Content = stack;
+            grid.Children.Add(btn);
+
+            return grid;
+        }
     }
 }
--- Engine/UserControls/Files.xaml.cs
+++ Engine/UserControls/Files.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Editor.Controls;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -9,11 +11,26 @@ namespace Editor.UserControls
     {
         public Grid ChangeColorWithTheme;
 
+        internal FilesController _filesControl = new FilesController();
+
         public Files()
         {
             this.InitializeComponent();
 
             ChangeColorWithTheme = x_Grid_ChangeColorWithTheme;
+
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Scenes", "\xEA86"));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Scripts", Symbol.Document));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Prefabs", "\xE734"));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Models", "\xF158"));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Animation", "\xE805"));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Materials", "\xF156"));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Textures", "\xEB9F"));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Audio", Symbol.Audio));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Videos", Symbol.Video));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Fonts", Symbol.Font));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Documents", "\xEA90"));
+            x_WrapPanel_Files.Children.Add(_filesControl.CategoryTile("Packages", "\xE74C", false));
         }
     }
 }
