f424f6d2663ea284a0999b70f2900e7e52c5e4cc
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2022-11-08 01:29:17 +0100

    Small changes to shader

--- Engine/Assets/Engine/Resources/Shader/SimpleLit.hlsl
+++ Engine/Assets/Engine/Resources/Shader/SimpleLit.hlsl
@@ -3,6 +3,7 @@ cbuffer ViewConstantsBuffer : register(b0)
     float4x4 ViewProjection;
     float3 Camera;
 };
+
 cbuffer PerModelConstantBuffer : register(b1)
 {
     float4x4 World;
@@ -15,18 +16,17 @@ struct appdata
     float3 normal : NORMAL;
 };
 
-Texture2D ObjTexture : register(t0);
-SamplerState ObjSamplerState : register(s0);
-
 struct VS_OUTPUT
 {
     float4 pos : SV_POSITION;
     float3 worldPos : POSITION;
-    float3 camPos : POSITION1;
+    float3 cameraPos : POSITION1;
     float2 uv : TEXCOORD;
     float3 normal : NORMAL;
 };
 
+Texture2D ObjTexture : register(t0);
+SamplerState ObjSamplerState : register(s0);
 
 VS_OUTPUT VS(appdata v)
 {
@@ -35,7 +35,7 @@ VS_OUTPUT VS(appdata v)
     o.pos = mul(float4(v.vertex, 1), mul(World, ViewProjection));
     o.normal = mul(float4(v.normal, 0), World);
     o.worldPos = mul(float4(v.vertex, 1), World);
-    o.camPos = Camera;
+    o.cameraPos = Camera;
     o.uv = v.uv;
 
     return o;
