c42ff94788c735a36ba5047507435ecb7513dead
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2022-11-08 01:29:36 +0100

    Added a shader category

--- Engine/UserControls/Files.xaml.cs
+++ Engine/UserControls/Files.xaml.cs
@@ -24,16 +24,17 @@ namespace Editor.UserControls
             _filesControl.CreateCatergoryTiles(
                 new Category() { Name = "Scenes", Glyph = "\xEA86", FileTypes = new string[] { ".usd", ".usda", ".usdc", ".usdz" }, Creatable = true },
                 new Category() { Name = "Scripts", Symbol = Symbol.Document, FileTypes = new string[] { ".cs" }, Creatable = true },
-                new Category() { Name = "Prefabs", Glyph = "\xE734", FileTypes = new string[] { ".prefab" }, PreviewTile = true },
-                new Category() { Name = "Models", Glyph = "\xF158", FileTypes = new string[] { ".fbx", ".obj", ".blend", ".3ds", ".dae" }, PreviewTile = true },
+                new Category() { Name = "Prefabs", Glyph = "\xE734", FileTypes = new string[] { ".prefab" }, Thumbnail = true },
+                new Category() { Name = "Models", Glyph = "\xF158", FileTypes = new string[] { ".fbx", ".obj", ".blend", ".3ds", ".dae" }, Thumbnail = true },
                 new Category() { Name = "Animations", Glyph = "\xE805", FileTypes = new string[] { ".fbx", ".dae" } },
-                new Category() { Name = "Materials", Glyph = "\xF156", FileTypes = new string[] { ".mat" }, PreviewTile = true, Creatable = true },
-                new Category() { Name = "Textures", Glyph = "\xEB9F", FileTypes = new string[] { ".png", ".jpg", ".jpeg", ".tiff", ".tga", ".psd", ".bmp", }, PreviewTile = true },
+                new Category() { Name = "Materials", Glyph = "\xF156", FileTypes = new string[] { ".mat" }, Thumbnail = true, Creatable = true },
+                new Category() { Name = "Textures", Glyph = "\xEB9F", FileTypes = new string[] { ".png", ".jpg", ".jpeg", ".tiff", ".tga", ".psd", ".bmp", }, Thumbnail = true },
                 new Category() { Name = "Audios", Symbol = Symbol.Audio, FileTypes = new string[] { ".m4a", ".mp3", ".wav", ".ogg" } },
-                new Category() { Name = "Videos", Symbol = Symbol.Video, FileTypes = new string[] { ".m4v", ".mp4", ".mov", ".avi" }, PreviewTile = true },
+                new Category() { Name = "Videos", Symbol = Symbol.Video, FileTypes = new string[] { ".m4v", ".mp4", ".mov", ".avi" }, Thumbnail = false },
                 new Category() { Name = "Fonts", Symbol = Symbol.Font, FileTypes = new string[] { ".ttf", ".otf" } },
+                new Category() { Name = "Shaders", Glyph = "\xE706", FileTypes = new string[] { ".hlsl" }, Creatable = true },
                 new Category() { Name = "Documents", Glyph = "\xF000", FileTypes = new string[] { ".txt", ".pdf", ".doc", ".docx" }, Creatable = true },
-                new Category() { Name = "Packages", Glyph = "\xE74C", FileTypes = new string[] { ".zip", ".7zip", ".rar" }, DefaultColor = true });
+                new Category() { Name = "Packages", Glyph = "\xE74C", FileTypes = new string[] { ".zip", ".7zip", ".rar" }, DefaultColor = false });
         }
 
         private void AppBarButton_Click_AddFiles(object sender, RoutedEventArgs e) { _filesControl.SelectFilesAsync(); }
