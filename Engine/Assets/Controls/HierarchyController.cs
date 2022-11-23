261eb286eaaaa4cb3c18fbbda78b1db2fee7df3f
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2022-11-23 03:02:17 +0100

    Removed obsolete helper code and fixed using namespaaces

--- Engine/Assets/Controls/HierarchyController.cs
+++ Engine/Assets/Controls/HierarchyController.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI;
+using Microsoft.UI.Text;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Input;
 using System;
-using Engine.Utilities;
-using Editor.UserControls;
 using System.Collections.Generic;
+using Editor.UserControls;
+using Engine.Utilities;
+using ExpandDirection = Microsoft.UI.Xaml.Controls.ExpandDirection;
+using Expander = Microsoft.UI.Xaml.Controls.Expander;
+using Orientation = Microsoft.UI.Xaml.Controls.Orientation;
 
 namespace Editor.Controls
 {
@@ -20,7 +28,7 @@ namespace Editor.Controls
         }
     }
 
-    internal class HierarchyController
+    internal partial class HierarchyController
     {
         public TreeView Tree;
 
@@ -73,6 +81,29 @@ namespace Editor.Controls
 
         public void DeselectTreeViewNodes() => Tree.SelectedNode = null;
 
+
+        private void List_OnAdd()
+        {
+            var entityList = Engine.Core.Instance.Scene.EntitytManager.EntityList;
+
+            var entity = entityList[^1];
+            var newEntry = new TreeEntry() { Name = entity.Name, ID = entity.ID };
+            if (entity.Parent != null)
+                newEntry.IDparent = entity.Parent.ID;
+
+            newEntry.Node = new TreeViewNode() { Content = newEntry, IsExpanded = true };
+            Hierarchy.Add(newEntry);
+
+            TreeEntry parent;
+            if ((parent = GetParent(newEntry)) is null)
+                Tree.RootNodes.Add(newEntry.Node);
+            else
+                parent.Node.Children.Add(newEntry.Node);
+        }
+    }
+
+    internal partial class HierarchyController
+    {
         public TreeEntry GetParent(TreeEntry node)
         {
             if (node.IDparent != null)
@@ -93,6 +124,7 @@ namespace Editor.Controls
 
             return list.ToArray();
         }
+
         public Entity GetEntity(TreeEntry node)
         {
             var engineObjectList = Engine.Core.Instance.Scene.EntitytManager.EntityList;
@@ -104,32 +136,128 @@ namespace Editor.Controls
             return null;
         }
 
-        private void List_OnAdd()
-        {
-            var entityList = Engine.Core.Instance.Scene.EntitytManager.EntityList;
-
-            var entity = entityList[^1];
-            var newEntry = new TreeEntry() { Name = entity.Name, ID = entity.ID };
-            if (entity.Parent != null)
-                newEntry.IDparent = entity.Parent.ID;
-
-            newEntry.Node = new TreeViewNode() { Content = newEntry, IsExpanded = true };
-            Hierarchy.Add(newEntry);
-
-            TreeEntry parent;
-            if ((parent = GetParent(newEntry)) is null)
-                Tree.RootNodes.Add(newEntry.Node);
-            else
-                parent.Node.Children.Add(newEntry.Node);
-        }
-
-        internal void SetNewParentTreeEntry(TreeViewNode newParent, params TreeViewNode[] treeViewNodes)
+        public void SetNewParentTreeEntry(TreeViewNode newParent, params TreeViewNode[] treeViewNodes)
         {
             foreach (var node in treeViewNodes)
             {
                 (newParent.Content as TreeEntry).IDparent = (node.Content as TreeEntry).ID;
                 GetEntity(node.Content as TreeEntry).Parent = GetEntity(newParent.Content as TreeEntry);
             }
+        }
+
+        private Grid CreateSeperator()
+        {
+            Grid grid = new Grid();
+
+            NavigationViewItemSeparator seperator = new NavigationViewItemSeparator() { Margin = new Thickness(10) };
+
+            grid.Children.Add(seperator);
+
+            return grid;
+        }
+
+        private Grid CreateTextFull(string s = "String")
+        {
+            TextBlock textInput = new TextBlock() { Text = s, Opacity = 0.5f, TextWrapping = TextWrapping.Wrap };
+
+            Grid grid = new Grid();
+            grid.Children.Add(textInput);
+
+            return grid;
+        }
+
+        private Grid CreateHeader(string s = "Header")
+        {
+            Grid grid = new Grid();
+            TextBlock header = new TextBlock() { Text = s, FontSize = 18, FontWeight = FontWeights.Bold, Margin = new Thickness(0, 20, 0, 0) };
+
+            grid.Children.Add(header);
+
+            return grid;
+        }
+
+        private Grid CreateSpacer()
+        {
+            Grid grid = new Grid() { Height = 10 };
+
+            return grid;
+        }
+
+        private Grid WrapInExpander(Grid content, string s = "Expander")
+        {
+            Grid grid = new Grid();
+            Expander expander = new Expander() { Header = s, HorizontalAlignment = HorizontalAlignment.Stretch, HorizontalContentAlignment = HorizontalAlignment.Left };
+
+            expander.Content = content;
+            grid.Children.Add(expander);
+
+            return grid;
+        }
+
+        private Grid CreateExpander(string s = "ExampleScript", params Grid[] properties)
+        {
+            Grid grid = new Grid() { Margin = new Thickness(0, 0, 0, 2) };
+            StackPanel stack = new StackPanel() { Orientation = Orientation.Vertical, Spacing = 10 };
+            Expander expander = new Expander() { Header = s, ExpandDirection = ExpandDirection.Down, HorizontalAlignment = HorizontalAlignment.Stretch, HorizontalContentAlignment = HorizontalAlignment.Left };
+            expander.IsExpanded = true;
+
+            foreach (var item in properties)
+                stack.Children.Add(item);
+
+            expander.Content = stack;
+            grid.Children.Add(expander);
+
+            return grid;
+        }
+
+        private Grid CreateExpanderWithToggleButton(string s = "ExampleScript", params Grid[] properties)
+        {
+            Grid grid = new Grid() { Margin = new Thickness(0, 0, 0, 2) };
+            StackPanel stack = new StackPanel() { Orientation = Orientation.Vertical, Spacing = 10 };
+            Expander expander = new Expander() { Header = s, ExpandDirection = ExpandDirection.Down, HorizontalAlignment = HorizontalAlignment.Stretch, HorizontalContentAlignment = HorizontalAlignment.Left };
+            expander.Header = new ToggleButton() { Content = s, IsChecked = true };
+
+            foreach (var item in properties)
+                stack.Children.Add(item);
+
+            expander.Content = stack;
+            grid.Children.Add(expander);
+
+            return grid;
+        }
+
+        private Grid CreateExpanderWithEditableHeader(string s = "ExampleScript", params Grid[] properties)
+        {
+            Grid grid = new Grid() { Margin = new Thickness(0, 0, 0, 2) };
+            StackPanel stack = new StackPanel() { Orientation = Orientation.Vertical, Spacing = 10 };
+            Expander expander = new Expander() { Header = s, ExpandDirection = ExpandDirection.Down, HorizontalAlignment = HorizontalAlignment.Stretch, HorizontalContentAlignment = HorizontalAlignment.Left };
+            expander.Header = new TextBox() { Text = s, Margin = new Thickness(0) };
+
+            foreach (var item in properties)
+                stack.Children.Add(item);
+
+            expander.Content = stack;
+            grid.Children.Add(expander);
+
+            return grid;
+        }
+
+        private Grid CreateButton(string s, TappedEventHandler tapped)
+        {
+            Grid grid = new Grid();
+
+            Button button = new Button() { Content = s, HorizontalAlignment = HorizontalAlignment.Center, Margin = new Thickness(10) };
+
+            button.Tapped += tapped;
+
+            AutoSuggestBox suggestBox = new AutoSuggestBox();
+
+            //FlyoutBase kbase = new FlyoutBase();
+            //button.Flyout = suggestBox;
+
+            grid.Children.Add(button);
+
+            return grid;
         }
     }
 }
