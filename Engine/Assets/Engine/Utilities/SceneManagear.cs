a559944d6b9387e127ff30f7b3fc45841df58990
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2022-12-06 14:26:17 +0100

    Added sceneManager

---
+++ Engine/Assets/Engine/Utilities/SceneManagear.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using Editor.Controller;
+using Engine.Components;
+using Engine.Editor;
+
+namespace Engine.Utilities
+{
+    internal class SceneManager
+    {
+
+    }
+}
