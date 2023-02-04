83d35bb410caee3cca66769c22f8b85b60d4b16f
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-02-04 20:09:04 +0100

    Updated ClassDiagram from pdf to png and small fixes

--- Engine/App.xaml.cs
+++ Engine/App.xaml.cs
@@ -21,33 +21,26 @@ namespace Editor
         {
             this.InitializeComponent();
 
-            try
-            {
-                var documentsDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                var rootPath = Path.Combine(documentsDir, "3DEngine");
-                var logFilePath = Path.Combine(rootPath, "Application.log");
+            var documentsDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var rootPath = Path.Combine(documentsDir, "3DEngine");
+            var logFilePath = Path.Combine(rootPath, "Application.log");
 
-                // Create directory
-                if(!Directory.Exists(rootPath))
-                    Directory.CreateDirectory(rootPath);
-                
-                // Create file 
-                if (!File.Exists(logFilePath))
-                    File.Create(logFilePath);
+            // Create directory
+            if (!Directory.Exists(rootPath))
+                Directory.CreateDirectory(rootPath);
 
-                // Reset log
+            // Reset log
+            if (File.Exists(logFilePath))
                 File.WriteAllText(logFilePath, String.Empty);
 
-                // Set up listener
-                FileStream traceLog = new(logFilePath, FileMode.Open);
-                TextWriterTraceListener listener = new(traceLog);
+            // Set up listener
+            FileStream traceLog = new(logFilePath, FileMode.OpenOrCreate);
+            TextWriterTraceListener listener = new(traceLog);
 
-                // Pass listener to trace
-                Trace.Listeners.Add(listener);
-                // Automatically write into file
-                Trace.AutoFlush = true;
-            }
-            catch { }
+            // Pass listener to trace
+            Trace.Listeners.Add(listener);
+            // Automatically write into file
+            Trace.AutoFlush = true;
 
             this.UnhandledException += (s, e) =>
             {
