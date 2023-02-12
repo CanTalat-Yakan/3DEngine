737ec21c07af7dbbf0763a493fd6dee7e9f22295
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-02-12 15:21:43 +0100

    Added Serialization Helper

---
+++ Engine/Assets/Controller/Helper/Serialization.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Editor.Controller
+{
+    internal enum EDataType
+    {
+        XML,
+        JSON,
+    }
+
+    static class Serialization
+    {
+        internal static void DeleteFile(string path)
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+        }
+
+        internal static void SaveFile<T>(T file, string path, EDataType eDataType = EDataType.XML)
+        {
+            switch (eDataType)
+            {
+                case EDataType.XML:
+                    SaveXml(file, path);
+                    break;
+                case EDataType.JSON:
+                    SaveJSON(file, path);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        internal static T LoadFile<T>(string path, EDataType eDataType = EDataType.XML)
+        {
+            object obj = null;
+
+            switch (eDataType)
+            {
+                case EDataType.XML:
+                    obj = LoadXml(typeof(T), path);
+                    break;
+                case EDataType.JSON:
+                    obj = LoadJSON(typeof(T), path);
+                    break;
+                default:
+                    break;
+            }
+
+            return (T)obj;
+        }
+
+        internal static void SaveXml<T>(T file, string path)
+        {
+            using (FileStream fs = new(path, File.Exists(path) ? FileMode.Create : FileMode.CreateNew))
+            {
+                XmlSerializer serializer = new(file.GetType());
+                serializer.Serialize(fs, file);
+            }
+        }
+
+        internal static void SaveJSON<T>(T file, string path)
+        {
+            JsonSerializerOptions options = new();
+            options.WriteIndented = true;
+
+            string data = JsonSerializer.Serialize(file, typeof(T), options);
+            File.WriteAllText(path, data);
+        }
+
+        internal static object LoadXml(Type type, string path)
+        {
+            object obj = null;
+
+            using (FileStream fs = new(path, FileMode.Open))
+            {
+                XmlSerializer serializer = new(type);
+                obj = serializer.Deserialize(fs);
+            }
+
+            return obj;
+        }
+
+        internal static object LoadJSON(Type type, string path)
+        {
+            return JsonSerializer.Deserialize(File.ReadAllText(path), type);
+        }
+    }
+}
