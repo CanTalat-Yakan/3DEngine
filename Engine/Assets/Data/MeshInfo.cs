f5f39fffeed7f1a7f242b0bdfd63bf21ca3c17cb
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-09-26 22:21:51 +0200

    Clean up

--- Engine/Assets/Data/MeshInfo.cs
+++ Engine/Assets/Data/MeshInfo.cs
@@ -1,5 +1,13 @@
 ﻿namespace Engine.Data;
 
+public struct Vertex
+{
+    public Vector3 Position;
+    public Vector2 TexCoord;
+    public Vector3 Normal;
+    public Vector3 Tangent;
+}
+
 public struct MeshInfo
 {
     public Vertex[] Vertices;
