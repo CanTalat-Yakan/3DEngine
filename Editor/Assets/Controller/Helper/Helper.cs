5ac6b552bdb4e8ccc1dd26e743cc3faf86074e24
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-10-04 17:23:20 +0200

    Added Color Field Back

--- Editor/Assets/Controller/Helper/Helper.cs
+++ Editor/Assets/Controller/Helper/Helper.cs
@@ -135,8 +135,8 @@ internal partial class Helper
 
     internal static Grid CreateColorButton(byte r = 0, byte g = 0, byte b = 0, byte a = 0)
     {
-        Windows.UI.Color col = new();
-        col.R = r; col.G = g; col.B = b; col.A = a;
+        Windows.UI.Color col = new() { R = r, G = g, B = b, A = a};
+
         ColorPickerButton colbutton = new() { SelectedColor = col };
 
         Style stylee = new() { TargetType = typeof(ColorPicker) };
