1a9c25bdd41dde1171a1f9473b96e0b6c55fd738
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-10-24 11:50:04 +0200

    ‎

--- Engine/Assets/Rendering/Data/ConstantBuffer.cs
+++ Engine/Assets/Rendering/Data/ConstantBuffer.cs
@@ -10,21 +10,3 @@ public struct PerModelConstantBuffer
 {
     public Matrix4x4 ModelView;
 }
-
-//public struct DirectionalLightConstantBuffer
-//{
-//    public Vector3 Direction;
-//    public float Pad;
-//    public Vector4 Diffuse;
-//    public Vector4 Ambient;
-//    public float Intensity;
-//}
-
-//public struct PointLightConstantBuffer
-//{
-//    public Vector3 Position;
-//    public float Pad;
-//    public Vector4 Diffuse;
-//    public float Intensity;
-//    public float Radius;
-//}
