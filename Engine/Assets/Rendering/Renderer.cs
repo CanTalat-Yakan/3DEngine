c266c36ccc4e5110ad8972e26b53ae5b08d4b76e
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-10-26 16:04:02 +0200

    ‎

--- Engine/Assets/Rendering/Renderer.cs
+++ Engine/Assets/Rendering/Renderer.cs
@@ -5,7 +5,6 @@ using Vortice.Direct3D12;
 using Vortice.Direct3D12.Debug;
 using Vortice.Direct3D;
 using Vortice.DXGI;
-using Vortice.DXGI.Debug;
 using Vortice.Mathematics;
 
 namespace Engine.Rendering;
