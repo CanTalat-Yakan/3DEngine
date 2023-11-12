4b1fae5e2c3084ac8a831fb04445be8ec54a6623
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-11-12 19:58:24 +0100

    ‎

--- Engine/Assets/Editor/Paths.cs
+++ Engine/Assets/Editor/Paths.cs
@@ -2,10 +2,9 @@
 
 public sealed class Paths
 {
-    public static readonly string DIRECTORY = AppContext.BaseDirectory;
+    public static readonly string DIRECTORY =   AppContext.BaseDirectory;
 
-    public static readonly string ASSETS = DIRECTORY + @"Assets\";
-
+    public static readonly string ASSETS =      DIRECTORY + @"Assets\";
     public static readonly string RESOURCES =   ASSETS + @"Resources\";
 
     public static readonly string TEMPLATES =   RESOURCES + @"Templates\";
