f232a4f0f7b6f26eeb24359c0c0e48de6a86026f
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-11-13 12:20:12 +0100

    ­

--- Engine/Assets/Rendering/Buffer/CameraBuffer.cs
+++ Engine/Assets/Rendering/Buffer/CameraBuffer.cs
@@ -35,7 +35,7 @@ public sealed unsafe class CameraBuffer
 
     public void Setup()
     {
-        //Renderer.Data.CommandList.ResourceBarrierTransition(_view, ResourceStates.VertexAndConstantBuffer, ResourceStates.Common);
+        //Renderer.Data.CommandList.ResourceBarrierTransition(_view, ResourceStates.GenericRead, ResourceStates.Common);
 
         // Set the constant buffer in the vertex shader stage of the device context.
         Renderer.Data.CommandList.SetGraphicsRootConstantBufferView(0, _view.GPUVirtualAddress);
