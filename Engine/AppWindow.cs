ae49e94a80e2c1977d380e51970c6f415f493643
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2023-11-17 00:16:15 +0100

    ­

--- Engine/AppWindow.cs
+++ Engine/AppWindow.cs
@@ -101,7 +101,7 @@ public sealed partial class AppWindow
     public bool ProcessMessage(uint msg, UIntPtr wParam, IntPtr lParam)
     {
         ImGui.SetCurrentContext(_imGuiContext);
-        if (_imGuiInputHandler is not null && _imGuiInputHandler.ProcessMessage((WindowMessage)msg, wParam, lParam))
+        if (_imGuiInputHandler.ProcessMessage((WindowMessage)msg, wParam, lParam))
             return true;
 
         switch ((WindowMessage)msg)
