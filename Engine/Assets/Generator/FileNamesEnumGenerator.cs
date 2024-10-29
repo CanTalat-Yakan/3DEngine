1efa917a38c5d228025c5790b4a942f7e346e4b7
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2024-10-29 10:44:05 +0100

    ⠀

---
+++ Engine/Assets/Generator/FileNamesEnumGenerator.cs
@@ -0,0 +1,60 @@
+﻿namespace Engine;
+
+// Enum for Scene files
+public enum SceneFiles
+{
+    teapot,
+}
+
+// Enum for Shader files
+public enum ShaderFiles
+{
+    ImGui,
+    SimpleLit,
+    Sky,
+    Unlit,
+}
+
+// Enum for Compute Shader files
+public enum ComputeShaderFiles
+{
+    // No files found
+}
+
+// Enum for Texture files
+public enum TextureFiles
+{
+    Default,
+    Transparent,
+    UVMap,
+}
+
+// Enum for Model files
+public enum ModelFiles
+{
+    Cube,
+    Cylinder,
+    Duck,
+    Icosphere,
+    Plane,
+    Quad,
+    Sphere,
+    Suzanne,
+    Torus,
+    Tree,
+}
+
+// Enum for Primitive files
+public enum PrimitiveFiles
+{
+    Cube,
+    Cylinder,
+    Duck,
+    Icosphere,
+    Plane,
+    Quad,
+    Sphere,
+    Suzanne,
+    Torus,
+    Tree,
+}
