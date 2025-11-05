562ceaa8b8bb409feb5a9d824029134995ac97f6
CanTalat-Yakan <can.talat.yakan@gmail.com>
2025-11-05 17:02:32 +0100

    Add initial project structure with .gitignore, solution, and main program

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using SDL3;
+
+namespace Engine;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        if (!SDL.Init(SDL.InitFlags.Video))
+        {
+            SDL.LogError(SDL.LogCategory.System, $"SDL could not initialize: {SDL.GetError()}");
+            return;
+        }
+
+        if (!SDL.CreateWindowAndRenderer("SDL3 Create Window", 800, 600, 0, out var window, out var renderer))
+        {
+            SDL.LogError(SDL.LogCategory.Application, $"Error creating window and rendering: {SDL.GetError()}");
+            return;
+        }
+
+        SDL.SetRenderDrawColor(renderer, 100, 149, 237, 0);
+
+        var loop = true;
+
+        while (loop)
+        {
+
+            while (SDL.PollEvent(out var e))
+            {
+                if ((SDL.EventType)e.Type == SDL.EventType.Quit)
+                {
+                    loop = false;
+                }
+            }
+
+            SDL.RenderClear(renderer);
+            SDL.RenderPresent(renderer);
+        }
+
+        SDL.DestroyRenderer(renderer);
+        SDL.DestroyWindow(window);
+
+        SDL.Quit();
+    }
+}
