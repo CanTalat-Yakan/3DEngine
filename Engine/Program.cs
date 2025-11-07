16db0e5de7e611f31160c258c476741a64057113
CanTalat-Yakan <can.talat.yakan@gmail.com>
2025-11-07 10:00:09 +0100

    ⠀⠀⠀⠀⠀

--- Engine/Program.cs
+++ Engine/Program.cs
@@ -16,9 +16,6 @@ public sealed class Program
     public struct FpsOverlay
     {
         private static double _highestFps;        
-        private static double _lowestFps;        
-        private static double _highestDelta;        
-        private static double _lowestDelta;        
         
         [OnRender]
         public static void Draw(BehaviorContext ctx)
@@ -26,29 +23,11 @@ public sealed class Program
             ImGui.Begin("HUD");
             var time = ctx.Res<Time>();
             var delta = time.DeltaSeconds;
-            var fps = 1.0 / time.DeltaSeconds;
-            if (_lowestFps == 0) _lowestFps = fps;
-            if (_lowestDelta == 0) _lowestDelta = delta;
-            if (delta > _highestDelta) _highestDelta = delta;
-            if (delta < _lowestDelta) _lowestDelta = delta;
+            var fps = 1.0 / delta;
             if (fps > _highestFps) _highestFps = fps;
-            if (fps < _lowestFps) _lowestFps = fps;
             ImGui.Text($"FPS: {fps:0}");
-            ImGui.Text($"Delta: {delta}");
-            ImGui.Text($"LowestFPS: {_lowestFps:0}");
             ImGui.Text($"HighestFPS: {_highestFps:0}");
-            ImGui.Text($"LowestDelta: {_lowestDelta}");
-            ImGui.Text($"HighestDelta: {_highestDelta}");
-            
-            // Every second reset min/max
-            if (time.ElapsedSeconds % 1.0 < time.DeltaSeconds)
-            {
-                _highestFps = 0;
-                _lowestFps = fps;
-                _highestDelta = 0;
-                _lowestDelta = delta;
-            }
-            
+            if (time.ElapsedSeconds % 1.0 < delta) _highestFps = 0;
             ImGui.End();
         }
     }
