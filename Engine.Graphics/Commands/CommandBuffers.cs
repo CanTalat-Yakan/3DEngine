b767b75d6637681c99735b5391eb2e4ec9bb5e39
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2026-03-30 20:15:38 +0200

    refactor: place files in root for improved organization

--- Engine.Graphics/Commands/CommandBuffers.cs
+++
@@ -1,6 +0,0 @@
-namespace Engine;
-
-/// <summary>Placeholder for command buffer allocation helpers.</summary>
-public static class CommandBuffers
-{
-}
--- Engine.Graphics/Pipeline/Pipeline.cs
+++
@@ -1,9 +0,0 @@
-namespace Engine;
-
-/// <summary>Legacy pipeline placeholder; real pipelines are managed by GraphicsDevice.</summary>
-public sealed class Pipeline : IPipeline
-{
-    public void Dispose()
-    {
-    }
-}
