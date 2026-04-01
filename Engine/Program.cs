347e9f93b8662eba6fcd661d1b89cb3089bc7901
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2026-04-01 02:34:39 +0200

    feat: update window title for 3D Engine

--- Engine/Program.cs
+++ Engine/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Engine;
 
 var config = Config.GetDefault(
-    title: "3D Engine Editor",
+    title: "3D Engine",
     width: 1280,
     height: 720,
     graphics: GraphicsBackend.Vulkan);
