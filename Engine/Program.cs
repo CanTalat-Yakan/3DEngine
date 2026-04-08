bd8941400b82b47a018d24bcaecf460ae2b27f3b
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2026-04-09 01:55:55 +0200

    feat: add TriangleMeshTest behavior to spawn camera and mesh entities

--- Engine/Program.cs
+++ Engine/Program.cs
@@ -1,4 +1,5 @@
-﻿using Engine;
+﻿using System.Numerics;
+using Engine;
 
 var config = Config.GetDefault(
     title: "3D Engine",
@@ -10,3 +11,22 @@ new App(config)
     .AddPlugin(new DefaultPlugins())
     .AddPlugin(new WebViewPlugin())
     .Run();
+
+[Behavior]
+public struct TriangleMeshTest
+{
+    [OnStartup]
+    public static void Start(BehaviorContext ctx)
+    {
+        var world = ctx.World;
+        var ecs = world.Resource<EcsWorld>();
+        var e = ecs.Spawn();
+        ecs.Add(e, new Camera(fovY: 60f * (float)(Math.PI / 180.0), near: 0.1f, far: 1000f));
+        ecs.Add(e, new Transform(new Vector3(0, 0, 5)));
+
+        var mesh = ecs.Spawn();
+        ecs.Add(mesh, new Mesh(new[] { new Vector3(0,1,0), new Vector3(-1,-1,0), new Vector3(1,-1,0) }));
+        ecs.Add(mesh, new Material(new Vector4(1, 1, 1, 1))); // white triangle
+        ecs.Add(mesh, new Transform(Vector3.Zero));
+    }
+}
